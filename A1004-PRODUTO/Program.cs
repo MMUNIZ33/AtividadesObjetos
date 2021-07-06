f34c41271e4db854b0da98f79171ae4156ec196a
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-06 19:49:07 -0400

    add A1004

---
+++ A1004-PRODUTO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1004_PRODUTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
