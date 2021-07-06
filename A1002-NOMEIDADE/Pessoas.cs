e4275042aeeb22b68239d48fc3c37cd24ad09334
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-05 21:50:11 -0400

    concluido A1002

---
+++ A1002-NOMEIDADE/Pessoas.cs
@@ -0,0 +1,8 @@
+namespace A1002_NOMEIDADE
+{
+    public class Pessoas
+    {
+        public string name;
+        public int idade;
+    }
+}
--- A1002-NOMEIDADE/Program.cs
+++ A1002-NOMEIDADE/Program.cs
@@ -6,7 +6,36 @@ namespace A1002_NOMEIDADE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pessoas A, B;
+            A = new Pessoas();
+            B = new Pessoas();
+
+            Console.WriteLine("Dados da primera pessoa:");
+            Console.Write("Nome: ");
+            A.name = Console.ReadLine();
+            Console.Write("Idade: ");
+            A.idade = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            B.name = Console.ReadLine();
+            Console.Write("Idade: ");
+            B.idade = int.Parse(Console.ReadLine());
+
+            if (A.idade > B.idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + A.name);
+            }
+            else if (A.idade < B.idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + B.name);
+            }
+            else
+            {
+                Console.WriteLine("Eles tem a mesma idade");
+            }
+
+
         }
     }
 }
