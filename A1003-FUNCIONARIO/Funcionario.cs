e77439925d7a8c5f1af6f4f4d919d11d89269cd7
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-05 22:08:35 -0400

    concluido A1003

---
+++ A1003-FUNCIONARIO/Funcionario.cs
@@ -0,0 +1,8 @@
+namespace A1003_FUNCIONARIO
+{
+    public class Funcionario
+    {
+        public string name;
+        public double salary;
+    }
+}
--- A1003-FUNCIONARIO/Program.cs
+++ A1003-FUNCIONARIO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace A1003_FUNCIONARIO
 {
@@ -6,7 +7,28 @@ namespace A1003_FUNCIONARIO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            Funcionario A, B;
+            A = new Funcionario();
+            B = new Funcionario();
+
+            // Funcionário 1
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            A.name = Console.ReadLine();
+            Console.Write("Salário: ");
+            A.salary = double.Parse(Console.ReadLine(), CI);
+            
+            // Funcionário 2
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            B.name = Console.ReadLine();
+            Console.Write("Idade: ");
+            B.salary = double.Parse(Console.ReadLine(), CI);
+
+            var media = 0.0;
+            media = (A.salary + B.salary) / 2.0;
+            Console.WriteLine("Salário médio: " + media.ToString("F2", CI));
         }
     }
 }
