008068d3481e7c4a9998e87342e646982b4acbb3
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-07 14:26:52 -0400

    concluido A1005

--- A1005-RETANGULO/Program.cs
+++ A1005-RETANGULO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace A1005_RETANGULO
 {
@@ -6,7 +7,16 @@ namespace A1005_RETANGULO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            Retangulo A = new Retangulo();
+            Console.WriteLine("Entre com a Largura e Altura do retângulo:");
+            A.Largura = double.Parse(Console.ReadLine(), CI);
+            A.Altura = double.Parse(Console.ReadLine(), CI);
+
+            // Calcular Área
+            Console.WriteLine("ÁREA: " + A.Area().ToString("F2", CI));
+            Console.WriteLine("PERÍMETRO: " + A.Perimetro().ToString("F2", CI));
+            Console.WriteLine("DIAGONAL: " + A.Diagonal().ToString("F2", CI));
         }
     }
 }
---
+++ A1005-RETANGULO/Retangulo.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace A1005_RETANGULO
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return (Largura + Altura) * 2;
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}
