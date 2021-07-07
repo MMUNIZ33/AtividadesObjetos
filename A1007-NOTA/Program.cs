837e0437382ff4844456b8445b8b547a3def8b0d
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-07 18:44:35 -0400

    add A1007

---
+++ A1007-NOTA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1007_NOTA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
