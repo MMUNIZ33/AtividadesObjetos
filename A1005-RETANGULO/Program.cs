4113ba37f71b3bc5d3ba56a7d7c5d13beff23139
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-07 11:50:17 -0400

    add A1005

---
+++ A1005-RETANGULO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1005_RETANGULO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
