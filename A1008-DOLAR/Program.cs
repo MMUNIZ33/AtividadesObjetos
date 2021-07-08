cc9dd232b9a1f7fe53fdeacc2d2cc597a08c3454
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-07 20:01:04 -0400

    add A1008

---
+++ A1008-DOLAR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1008_DOLAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
