70e9b642aa6252e6b5f320cc6237a2e986ef1170
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-07 23:12:21 -0400

    add A1009

---
+++ A1009-STATIC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1009_STATIC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
