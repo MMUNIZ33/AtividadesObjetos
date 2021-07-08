a3a499b7f1cbc903c121457cff08b800520c528d
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-07 23:41:54 -0400

    versão 1

--- A1009-STATIC/Program.cs
+++ A1009-STATIC/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Globalization;
 
 namespace A1009_STATIC
 {
     class Program
     {
+        static double Pi = 3.14;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            Console.Write("Valor do Raio: ");
+            double raio = double.Parse(Console.ReadLine(), CI);
+
+            double circ = Circunferencia(raio);
+            double volume = Volume(raio);
+
+            Console.WriteLine("CIRCUNFERÊNCIA: " + circ.ToString("F2", CI));
+            Console.WriteLine("VOLUME: " + volume.ToString("F2", CI));
+            Console.WriteLine("VALOR DE PI: " + Pi.ToString("F2", CI));
+
+        }
+
+        static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
         }
     }
 }
