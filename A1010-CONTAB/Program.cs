ed69a57a92a064ef47b8fb7c1426a858c94f3661
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-18 00:55:27 -0400

    add A1010

---
+++ A1010-CONTAB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1010_CONTAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
