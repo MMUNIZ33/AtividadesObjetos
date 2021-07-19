ac5c2ecb3327b688ba852ca14f3af7ef928ea14c
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-18 22:48:47 -0400

    concluido A1011

--- A1011-VET/Program.cs
+++ A1011-VET/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace A1011_VET
 {
@@ -6,7 +7,21 @@ namespace A1011_VET
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // APENAS REVISÃO
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            double soma = 0.0;
+
+            int N = int.Parse(Console.ReadLine());
+            double[] x = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                x[i] = double.Parse(Console.ReadLine(), CI);
+                soma += x[i];
+            }
+
+            double media = soma / N;
+            Console.WriteLine("ALTURA MÉDIA = " + media.ToString("F2", CI));
         }
     }
 }
