e269d5ef39e905db58cd851fc83a3b1e8ebfb643
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-18 22:24:09 -0400

    add A1011

---
+++ A1011-VET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1011_VET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
