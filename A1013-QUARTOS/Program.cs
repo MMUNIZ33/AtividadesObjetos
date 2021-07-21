e394396cdf23d7955d2f43ccd87696d0b99f55fe
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-21 02:35:12 -0400

    add A1013

---
+++ A1013-QUARTOS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1013_QUARTOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
