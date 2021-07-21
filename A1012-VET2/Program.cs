5da576b90b8e9579fc448c670b71289e27cf87c4
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-21 02:12:48 -0400

    add A1012

---
+++ A1012-VET2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1012_VET2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
