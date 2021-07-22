a7e683d88eafaea010c7ac334115bedb758df94a
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-21 23:46:53 -0400

    add A1014

---
+++ A1014-PARAMS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1014_PARAMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
