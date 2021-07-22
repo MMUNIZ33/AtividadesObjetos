0fb6de8fd7761942fbed5d2b62545fabb48feca5
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-22 00:35:36 -0400

    ref e out

--- A1014-PARAMS/Calculator.cs
+++ A1014-PARAMS/Calculator.cs
@@ -2,6 +2,13 @@ namespace A1014_PARAMS
 {
     public class Calculator
     {
+        // // ref e out são considerados "code smells" (design ruim). É bom evitar usá-los.
+        // // return é mais indicado.
+        // public static void Triple(int x, out int y)
+        // {
+        //     y = x * 3;
+        // }
+        
         public static int Soma(params int[] numero)
         {
             var soma = 0;
--- A1014-PARAMS/Program.cs
+++ A1014-PARAMS/Program.cs
@@ -6,6 +6,13 @@ namespace A1014_PARAMS
     {
         static void Main(string[] args)
         {
+            // // ref e out são considerados "code smells" (design ruim). É bom evitar usá-los.
+            // // return é mais indicado.
+            // int a = 1;
+            // int b;
+            // Calculator.Triple(a, out b);
+            // Console.WriteLine(b);
+            
             int s1 = Calculator.Soma(1, 2, 3);
             int s2 = Calculator.Soma(10, 5, 5);
             Console.WriteLine(s1);
