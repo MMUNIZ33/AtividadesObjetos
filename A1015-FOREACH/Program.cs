0d732397cb0c2dae2d469a79b9978fcaf805b5df
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-22 00:50:14 -0400

    foreach

---
+++ A1015-FOREACH/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace A1015_FOREACH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = new string[] { "Matheus", "Felipe", "James", "Lilian"};
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+
+            Console.WriteLine("---------------------------------");
+
+            foreach (var item in vet)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
