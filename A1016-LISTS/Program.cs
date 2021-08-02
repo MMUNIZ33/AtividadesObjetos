1537b285e293a5eb59f14c5cb17fdaa092aec700
Matheus Muniz <matthews.muniz33@gmail.com>
2021-08-02 01:01:38 -0400

    add A1016

---
+++ A1016-LISTS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1016_LISTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
