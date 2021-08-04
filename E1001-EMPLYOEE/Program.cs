32a45a0d92ac4391263b88c2a5c4bd9a01a7b8af
Matheus Muniz <matthews.muniz33@gmail.com>
2021-08-03 23:13:19 -0400

    add E1001

---
+++ E1001-EMPLYOEE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E1001_EMPLYOEE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
