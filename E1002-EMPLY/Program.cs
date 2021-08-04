42c5e668e1395edc10554715f23104bb118e652d
mmuniz33 <matthews.muniz33@gmail.com>
2021-08-04 14:02:25 -0400

    add E1002

---
+++ E1002-EMPLY/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E1002_EMPLY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
