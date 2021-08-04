916db5dfdf23d744cde6e4e00fe47e4321c1b845
mmuniz33 <matthews.muniz33@gmail.com>
2021-08-04 15:55:45 -0400

    concluido E1001

--- E1002-EMPLY/Funcionario.cs
+++ E1002-EMPLY/Funcionario.cs
@@ -1,14 +1,29 @@
+using System.Globalization;
+
 namespace E1002_EMPLY
 {
     class Funcionario
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public double Salary {get; set private; }
+        public double Salary { get; private set; }
 
-        public Funcionario(int id, string name, double Salary)
+        public Funcionario(int id, string name, double salary)
         {
-            
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void AlterarSalary(double porcentagem)
+        {
+            Salary += Salary * porcentagem / 100.0;
+        }
+
+        public override string ToString()
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            return ($"{Id}, {Name}, {Salary.ToString("F2", CI)}");
         }
     }
 }
