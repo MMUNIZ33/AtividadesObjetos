f8532a18b5fc7dce3a2708389c86a28582719f7e
mmuniz33 <matthews.muniz33@gmail.com>
2021-08-05 13:22:48 -0400

    add A1016

---
+++ A1016-DIAGONAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1016_DIAGONAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
