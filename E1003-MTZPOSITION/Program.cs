4d73321c322e755bb5eb81883523b4acd3e292ea
mmuniz33 <matthews.muniz33@gmail.com>
2021-08-05 16:34:05 -0400

    inc

--- E1003-MTZPOSITION/Program.cs
+++ E1003-MTZPOSITION/Program.cs
@@ -6,7 +6,57 @@ namespace E1003_MTZPOSITION
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n, L, C;
+            int[,] matrz;
+            string[] s;
+            // Proporção da matriz
+            s = Console.ReadLine().Split(' ');
+            L = int.Parse(s[0]);
+            C = int.Parse(s[1]);
+            matrz = new int[L, C];
+
+            // Leitura da matriz
+            for (int i = 0; i < L; i++)
+            {   
+                s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < C; j++)
+                {
+                    matrz[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < L; i++)
+            {
+                for (int j = 0; j < C; j++)
+                {
+                    if (n == matrz[i, j])
+                    {
+                        Console.Write("Posição: ");
+                        Console.WriteLine($"{i},{j}");
+                        
+                        // refazer as condições
+                        if (j > 0)
+                        {
+                            Console.Write("Esquerda: " + matrz[i, (j + 1)]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Direita: " + matrz[i, j]);
+                        }
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Em cima: ");
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Em baixo: ");
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
